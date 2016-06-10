df6a71fa3bf819f0858ee85ef9862f808defebd3
hexufang <fhx35@hotmail.com>
2016-06-10 16:10:45 +0800

    集成log4j

--- src/main/java/com/hexu/springboottest/controller/MessageController.java
+++ src/main/java/com/hexu/springboottest/controller/MessageController.java
@@ -2,6 +2,7 @@ package com.hexu.springboottest.controller;
 
 import com.hexu.springboottest.model.MessageInfo;
 import com.hexu.springboottest.service.MessageService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/message")
 public class MessageController {
 
+    private static Logger logger = Logger.getLogger(MessageController.class);
+
     @Autowired
     private MessageService messageService;
 
@@ -21,5 +24,16 @@ public class MessageController {
         MessageInfo messageInfo=messageService.getMessage().get(0);
         return messageInfo;
     }
+
+    @RequestMapping("/logTest")
+    public MessageInfo logTest(){
+        MessageInfo messageInfo=messageService.getMessage().get(0);
+        logger.debug("this is debug");
+        logger.info("this is info");
+        logger.error("this is error");
+        return messageInfo;
+    }
+
+
 
 }
